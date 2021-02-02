003fa646afe1015a618f4ffeef8ce3356bff3733
Andrea <andre93.lin@gmail.com>
2021-02-02 10:08:33 -0300

    Complete repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    let result = "";
+    if (times < 0) {
+        return "ERROR";
+    } else {
+        for (i = 0; i < times; i++) {
+            result += text;
+        }  
+        return result;
+    }
 }
 
 module.exports = repeatString
