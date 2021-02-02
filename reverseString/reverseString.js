c0d085103cc2ad36858456e938c2a7625ae7250c
Andrea <andre93.lin@gmail.com>
2021-02-02 10:31:52 -0300

    Complete reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(userText) {
+  let i = userText.length - 1;
+  let reversedUserText = [];
+  let result = "";
 
+  while (i >= 0) {
+      reversedUserText.push(userText[i]);
+      i--;
+  }
+
+  for (let letter in reversedUserText) {
+    result += reversedUserText[letter];
+  }
+
+  return result;
 }
 
 module.exports = reverseString
