921f457d5be60f4243e3c39817814612e2e6bee6
Andrea <andre93.lin@gmail.com>
2021-02-05 11:57:17 -0300

    Completed tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(inputTemperature) { 
+    let result = (inputTemperature - 32) * (5 / 9);
+    let rounded = Math.round(result * 10) / 10;
+    return rounded;
 }
 
-const ctof = function() {
-
+const ctof = function(inputTemperature) {
+    let result = (inputTemperature * (9 / 5)) + 32;
+    let rounded = Math.round(result * 10) / 10;
+    return rounded;
 }
 
 module.exports = {
