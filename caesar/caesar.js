e32f40f2a038aabf3bad6eda4bddec48d214551c
Andrea <andre93.lin@gmail.com>
2021-03-01 20:51:38 -0300

    adjusted comments in caesar exercise

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,31 +1,28 @@
 const caesar = function(string, keyNumber) {
     const regex = /[a-zA-Z]/g; //regular expression to find all alphabet characters
-    let encoded = string.replaceAll(regex, function(match) {
+    let encoded = string.replaceAll(regex, function(match) { // function to replace all input letters to encoded letter
         // translate from letter to unicode
         let index = 0;
         let unicode = match.charCodeAt(index);
 
         if (unicode >= 65 && unicode <= 90) {
-            unicode -= 65; // set unicode to 0-26
+            unicode -= 65; // Upper case letters, set unicode to 0-26
             unicode += keyNumber; // add keyNumber to shift unicode
-            unicode = ((unicode % 26) + 26) % 26; // apply modulo
+            unicode = ((unicode % 26) + 26) % 26; // apply modulo, using ((a % n ) + n ) % n, so that we allow negative numbers to work (and to do negative shifting).
             unicode += 65 // reset unicode to 65 - 90
         } else if (unicode >= 97 && unicode <= 122) {
-            unicode -= 97; // set unicode to 0 - 26
+            unicode -= 97; // Lower case letters, set unicode to 0 - 26
             unicode += keyNumber; // add keyNumber to shift unicode
             unicode = ((unicode % 26) + 26) % 26; // apply modulo
             unicode += 97; // reset unicode to 97 - 122
         }
 
-        // convert from encoded unicode to encoded alphabet characters
+        // convert from encoded unicode to encoded letter
         let result = String.fromCharCode(unicode);
         return result;
     });
     return encoded;
 }
-
-let encodedMessage = caesar('Hello, World!', 5);
-console.log(encodedMessage);
 
 module.exports = caesar
 
