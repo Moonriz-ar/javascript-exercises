68cd5388ecab788cc1d5ecb0a6a109d5a1dfe6a3
Andrea <andre93.lin@gmail.com>
2021-03-01 10:47:05 -0300

    finished calculator exercise"

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,36 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2; 
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum (array) {
+	let result = array.reduce((sum, current) => sum + current, 0);
+	return result;
 }
 
-function multiply () {
-	
+function multiply (array) {
+	let result = array.reduce((multiplied, current) => multiplied * current, 1);
+	return result;
 }
 
-function power() {
-	
+function power(num1, num2) {
+	return Math.pow(num1, num2);
 }
 
-function factorial() {
-	
+function factorial(num) {
+	let result = 1;
+	if (num === 0) {
+		return result;
+	} else {
+		while (num > 0) {
+			result *= num;
+			num--;
+		}
+		return result;
+	}
 }
 
 module.exports = {
