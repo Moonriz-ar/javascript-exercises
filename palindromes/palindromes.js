59823e564b36b85503eb8aa3f78ad9822108e1a4
Andrea <andre93.lin@gmail.com>
2021-03-01 12:02:00 -0300

    finished palindromes exercises

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    let clean = string.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    let reverse = "";
 
+    for (let i = clean.length - 1; i >= 0; i--) {
+        let letter = clean[i];
+        reverse += letter;
+    }
+
+   return (clean === reverse) ? true : false;
 }
+
+palindromes('Racecar!');
 
 module.exports = palindromes
