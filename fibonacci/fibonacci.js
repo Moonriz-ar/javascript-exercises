469f2efc830f2773e09c579cd354e8f59d727540
Andrea <andre93.lin@gmail.com>
2021-03-02 11:59:38 -0300

    finished fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let array = [0, 1];
+    if (number < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 0; i < number; i++) {
+            let firstPreceding = i;
+            let secondPreceding = i + 1;
+            let nextNumber = array[firstPreceding] + array[secondPreceding];
+            array.push(nextNumber);
+        }
+        return array[number];
+    }
 }
 
 module.exports = fibonacci
